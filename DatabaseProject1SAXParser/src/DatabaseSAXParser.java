b0483a3b1a595477fd681f5b06fa0d5ecf2e3975
ilia-iliev <ilia.iliev94@gmail.com>
2016-10-07 21:57:41 +0800

    Update DatabaseSAXParser.java

--- DatabaseProject1SAXParser/src/DatabaseSAXParser.java
+++ DatabaseProject1SAXParser/src/DatabaseSAXParser.java
@@ -36,7 +36,7 @@ public class DatabaseSAXParser {
 class UserHandler extends DefaultHandler {
 
 	boolean nonsense = false;
-	int primaryIndex = 1;
+	int primaryIndex = 0;
 	int authCount = 0;
 
 	File articlesFile = new File("articleFile.csv");
@@ -66,7 +66,8 @@ class UserHandler extends DefaultHandler {
 	@Override
 	public void startElement(String uri, String localName, String qName,
 			Attributes attributes) throws SAXException {
-		if (attributes.getLength()>0) {
+		if //(attributes.getLength() > 0) {
+		(isPublished(qName)){
 			newEntry();
 			element = qName;
 		} else if (qName.equals("author") || qName.equals("title")
@@ -75,13 +76,23 @@ class UserHandler extends DefaultHandler {
 		}
 
 	}
-	
+
+	private boolean isPublished(String s) {
+		if (s.equals("article") || s.equals("inproceedings")
+				|| s.equals("proceedings") || s.equals("book")
+				|| s.equals("incollection") || s.equals("phdthesis")
+				|| s.equals("mastersthesis") || s.equals("www")
+				|| s.equals("data"))
+			return true;
+		return false;
+	}
+
 	private void newEntry() {
-		chars.setLength(0);;
+		chars.setLength(0);
 		nonsense = false;
 		nextArticleLine[0] = primaryIndex + "";
 		primaryIndex++;
-		nextAuthorLine = new String[20];
+		nextAuthorLine = new String[1000];
 		authCount = 0;
 
 	}
@@ -90,28 +101,28 @@ class UserHandler extends DefaultHandler {
 	public void endElement(String uri, String localName, String qName)
 			throws SAXException {
 		if (qName.equals("author")) {
-			//enters here more times than it should!
+			// enters here more times than it should!
 			nextAuthorLine[authCount] = chars.toString();
 			authCount++;
-			if (authCount >= 20) {
-				authCount = 0;
-				System.out.println(primaryIndex + " ");
+			if (authCount >= 50) {
+				// authCount = 0;
+				System.out.println(primaryIndex + " " + localName);
 				nonsense = true;
 			}
 		} else if (qName.equals("year")) {
-			//needs to set nonsense to true if already appeared!
+			// needs to set nonsense to true if already appeared!
 			nextArticleLine[1] = chars.toString();
 		} else if (qName.equals("title")) {
 			nextTitleLine[0] = chars.toString();
 		} else if (qName.equals("journal")) {
 			nextArticleLine[2] = chars.toString();
-		} else if (!nonsense) {
-			if (qName.equals(element)) {
-				articleWriter.writeNext(nextArticleLine);
-				authorWriter.writeNext(nextAuthorLine);
-				titleWriter.writeNext(nextTitleLine);
-			}
+		} else // if (!nonsense) {
+		if (qName.equals(element)) {
+			articleWriter.writeNext(nextArticleLine);
+			authorWriter.writeNext(nextAuthorLine);
+			titleWriter.writeNext(nextTitleLine);
 		}
+		// }
 
 	}
 
